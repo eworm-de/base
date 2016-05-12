65dece3225a2918b4134c24f7598883716edc9f1
Tom Gundersen <teg@jklm.no>
2016-05-12 11:11:01 +0200

    devices: device - print debug output if we get unexpected data in REMOVE
    Then process the event as normal, just ignore the data.
    
    Signed-off-by: Tom Gundersen <teg@jklm.no>

--- src/devices/device.c
+++ src/devices/device.c
@@ -641,12 +641,10 @@ int device_from_nulstr(Manager *m, struct device **devicep, int *actionp,
         if (buflen < 0)
                 return buflen;
 
-/*
         if ((action == UEVENT_ACTION_REMOVE || action == UEVENT_ACTION_MOVE) && buflen > 0) {
-                fprintf(stderr, "unexpected data in '%s'\n", action_string);
-                return -EBADMSG;
+                fprintf(stderr, "unexpected data in '%s': %s\n", action_string, buf);
         }
-*/
+
         while (buflen > 0) {
                 const char *key, *value;
 
